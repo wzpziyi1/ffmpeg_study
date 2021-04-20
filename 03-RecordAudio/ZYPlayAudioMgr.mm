bc7b5f0c4102172c92d928433b05fb3db3a5751d
wangzhipan <wangzhipan@yy.com>
2021-04-20 19:45:24 +0800

    音频播放处理

--- 03-RecordAudio/ZYPlayAudioMgr.mm
+++ 03-RecordAudio/ZYPlayAudioMgr.mm
@@ -20,19 +20,16 @@ extern "C" {
 #define kSampleBufferCount 1024
 
 //一个缓冲区样本大小：位深度 * 声道数 / 8  个字节
-#define kSingleSampleBufferBytes (16 * kChannelCount) / 8
+#define kSingleSampleBufferBytes ((16 * kChannelCount) / 8)
 
 //缓冲区样本总大小，字节数
 #define kTotalSampleBufferBytes (kSingleSampleBufferBytes * kSampleBufferCount)
 
 typedef struct {
-    int len;
-    int pullLen;
-    UInt8 *data = nullptr;
+    int len = 0;
+    int pullLen = 0;
+    Uint8 *data = nullptr;
 }AudioBuffer;
-
-static UInt8 readData[kTotalSampleBufferBytes];
-static int readLen = 0;
 
 @implementation ZYPlayAudioMgr
 
@@ -52,6 +49,17 @@ void pullAudioCallback(void *userdata, Uint8 * stream, int len)
     SDL_memset(stream, 0, len);
     
     //取出
+    AudioBuffer *buffer = (AudioBuffer *)userdata;
+    
+    if (buffer->len == 0) return;
+    
+    buffer->pullLen = buffer->len < len ? buffer->len : len;
+    
+    SDL_MixAudio(stream, buffer->data, buffer->pullLen, SDL_MIX_MAXVOLUME);
+    
+    //一段buffer->data填充进去缓冲区了，得换下一段了
+    buffer->data += buffer->pullLen;
+    buffer->len -= buffer->pullLen;
 }
 
 - (void)playAudio
@@ -95,19 +103,36 @@ void pullAudioCallback(void *userdata, Uint8 * stream, int len)
     }
     NSFileHandle *fileHandle = [NSFileHandle fileHandleForReadingAtPath:kAudioPath];
     
+    static Uint8 readData[kTotalSampleBufferBytes];
+    
     while (true) {
-        NSData *data = [fileHandle readDataOfLength:kTotalSampleBufferBytes];
+        if (buffer.len > 0) {
+            SDL_Delay(1);
+            continue;
+        };
+        NSData *pcmData = [fileHandle readDataOfLength:kTotalSampleBufferBytes];
+        
         //读取到文件末尾了
-        if (data == nil || data.length == 0) {
+        //这里有问题，当音频缓冲区读完数据的时候，还没开始播放，就break了
+        //会导致最后读取的这部分音频数据不能播放
+        //所以，这里要推测还剩下多长播放时间，delay下，再退出
+        if (pcmData == nil || pcmData.length == 0) {
+            //还剩下多长播放时间：这一次读取的缓冲区数据 / 每秒缓冲区处理的数据
+            //转换成毫秒
+            int ms = buffer.pullLen * 1000 / kSingleSampleBufferBytes;
+            SDL_Delay(ms);
             break;
         }
-        readLen = (int)data.length;
-        [data getBytes:readData length:readLen];
-        [fileHandle seekToFileOffset:readLen];
-        while (readLen > 0) {
-            SDL_Delay(1);
-        }
+        buffer.len = (int)pcmData.length;
+        [pcmData getBytes:readData length:buffer.len];
+        buffer.data = readData;
+        [fileHandle seekToFileOffset:buffer.len];
     }
-     
+    
+    [fileHandle closeFile];
+    //关闭音频
+    SDL_CloseAudio();
+    //退出音频子系统
+    SDL_Quit();
 }
 @end
